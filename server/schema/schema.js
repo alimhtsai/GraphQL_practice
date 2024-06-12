703ececf92941de63f036ceae0c58443b712674e
alimhtsai <48788292+alimhtsai@users.noreply.github.com>
2024-06-11 18:16:01 -0700

    add schema

---
+++ server/schema/schema.js
@@ -0,0 +1,19 @@
+const graphql = require('graphql')
+
+const {
+    GraphQLObjectType,
+    GraphQLID,
+    GraphQLString,
+    GraphQLInt
+} = graphql
+
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    description: 'Documentation for user...',
+    fields: () => ({
+        id: {type: GraphQLID},
+        name: {type: GraphQLString},
+        age: {type: GraphQLInt}
+    })
+
+})
