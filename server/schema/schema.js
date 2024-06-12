180a8e42f5b46a8a99122a58a56779336dce7049
alimhtsai <48788292+alimhtsai@users.noreply.github.com>
2024-06-12 14:25:30 -0700

    add RootQuery and schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -4,16 +4,41 @@ const {
     GraphQLObjectType,
     GraphQLID,
     GraphQLString,
-    GraphQLInt
+    GraphQLInt,
+    GraphQLSchema
 } = graphql
 
+// create types
 const UserType = new GraphQLObjectType({
     name: 'User',
     description: 'Documentation for user...',
     fields: () => ({
-        id: {type: GraphQLID},
+        id: {type: GraphQLString},
         name: {type: GraphQLString},
         age: {type: GraphQLInt}
     })
+})
 
+// RootQuery
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    description: 'Description',
+    fields: {
+        user: {
+            type: UserType,
+            args: {
+                id: {
+                    type: GraphQLString
+                }
+            },
+            resolve(parent, args) {
+                // we resolve the data
+                // get and return data from a datasource
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 })
